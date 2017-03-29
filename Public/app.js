c8b64dfb20a493f716d62b2db944ee4f570b3728
Matthew DuBose <Matthew DuBose>
2017-03-29 13:12:39 -0500

    added in isolated scope

--- Public/app.js
+++ Public/app.js
@@ -26,3 +26,39 @@ angular.module('ui.bootstrap.demo').controller('AccordionDemoCtrl', function ($s
     isFirstDisabled: false
   };
 });
+angular.module('ui.bootstrap.demo').directive('helloWorld', function(){
+    return {
+                  template: 'Hello World'
+    };
+} ) ;
+angular.module('ui.bootstrap.demo').controller('CustomersController', function ($scope) {
+var injectparams = ['$scope'];
+
+$scope.tasks = [{title: 'Task 1'}];
+
+$scope.customer = {
+  name: 'David',
+  street: '1234 Anywhere St.'
+};
+
+});
+// scope {} = nothing passes through
+// @ string with one way binding
+// = two way data binding with strings / objects (changes made in directive to value passed in will change the parent as well)
+angular.module('ui.bootstrap.demo').directive('sharedScope', function(){
+    return {
+                  template: 'Name: {{customer.name}} Street: {{customer.street}}'
+    };
+} ) ;
+
+angular.module('ui.bootstrap.demo').directive('isolatedScope', function(){
+    return {        scope: { name: '@', street: '@'},
+                  template: 'Name: {{name}} Street: {{street}}'
+    };
+} ) ;
+
+angular.module('ui.bootstrap.demo').directive('isolatedScopeWithEquals', function(){
+    return {        scope: { datasource: '='},
+                  template: 'Name: {{datasource.name}} Street: {{datasource.street}}'
+    };
+} ) ;
