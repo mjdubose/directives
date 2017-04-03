cef33abc6d5f91acf7ec75a15eed9f7995086ebd
mjdubose <matthew.dubose@gmail.com>
2017-04-03 11:19:18 -0500

    ng Model

--- server.js
+++ server.js
@@ -9,7 +9,3 @@ app.use('/', express.static(path.join(__dirname, "../Public")));
 const port = process.env.PORT ? process.env.PORT : (process.env.NODE_ENV === 'test' ? 4000 : 3000);
 console.log('running on port', port);
 app.listen(port);
-//                                
-//http://media.blizzard.com/d3/icons/items/large/unique_gloves_set_02_p2_demonhunter_male.png 
-// icons are available for skills or items (items take large or small sizes, skills have pixel size allotments 21, 42 or 64);
-//.png name is stored in items or skills database table under icon.
